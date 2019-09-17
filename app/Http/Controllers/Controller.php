22f1ff4b874c873db326b3d7f63d0cc3819e7da5
bryan <40491783+bryan-k@users.noreply.github.com>
2019-09-17 19:17:36 +0300

    code for checking login added
    commented start to end of my code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,11 +6,52 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+//added this two lines down
+use Illuminate\Http\Request;
+use Validator;
+use Auth;
 
-class Controller extends BaseController
+class Controller extends Controller
+
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function index(){
         return view('login');
     }
+//login code
+    function checklogin(Request $request){
+        {
+            $this->validate($request, [
+             'email'   => 'required|email',
+             'password'  => 'required|alphaNum|min:8'
+            ]);
+       
+            $user_data = array(
+             'email'  => $request->get('email'),
+             'password' => $request->get('password')
+            );
+       
+            if(Auth::attempt($user_data))
+            {
+                return redirect('/home');
+            }
+            else
+            {
+             return back()->with('error', 'Wrong Login Details');
+            }
+       
+           }
+
+           function home ()
+           {
+            return view('home');
+           }
+       }
+    function logout()
+    {
+     Auth::logout();
+     return redirect('main');
+    }
+    //end of code required for login
 }
+    
+?>
