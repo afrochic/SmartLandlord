f05727d98798d17f294494af05ddb3acd9d0a569
dgikonyo <denniskimani05@gmail.com>
2019-09-23 11:40:11 +0300

    Full database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -14,10 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->bigIncrements('id');
+            $table->string('firstName');
+            $table->string('lastName');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('idNumber');
+            $table->integer('phoneNumber');
+            $table->string('gender');
+            $table->date('dateJoined');
             $table->rememberToken();
             $table->timestamps();
         });
