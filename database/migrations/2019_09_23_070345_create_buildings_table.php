331bb5820b548533f30aef30bc5a3bd734ef01e3
dgikonyo <denniskimani05@gmail.com>
2019-09-23 11:43:43 +0300

    Full database

---
+++ database/migrations/2019_09_23_070345_create_buildings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBuildingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('buildingName');
+            $table->string('city');
+            $table->date('dateEntered');
+            $table->integer('landTitleNumber');
+            $table->integer('numberOfHouses');
+            $table->integer('caretakerId');
+            $table->foreign('caretakerId')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buildings');
+    }
+}
